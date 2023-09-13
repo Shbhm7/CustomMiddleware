64f78ef54af4b7d607180f008a645be70811aff1
Shbhm7 <shbhmpathania@gmail.com>
2023-09-13 18:00:43 +0530

    Add project files.

---
+++ CustomExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace CustomMiddleware
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Threading.Tasks;
+
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CustomExceptionMiddleware> _logger;
+
+        public CustomExceptionMiddleware(RequestDelegate next, ILogger<CustomExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing the request.");
+                await ExceptionHandler.HandleExceptionAsync(context, ex);
+            }
+        }
+    }
+
+}
---
+++ ExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomMiddleware
+{
+    using Microsoft.AspNetCore.Http;
+    using System;
+    using System.Threading.Tasks;
+
+    public static class ExceptionHandler
+    {
+        public static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "An error occurred while processing your request.",
+                ExceptionMessage = exception.Message
+            });
+        }
+    }
+
+}
